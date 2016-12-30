40d0574074d84d75cac8706809db4cf972acfd72
Michelle Golle <michelleg54@gmail.com>
2016-12-30 12:34:58 -0700

    add problem and solution to each file

--- euler005.rb
+++ euler005.rb
@@ -1,3 +1,9 @@
+# 2520 is the smallest number that can be divided by each of the numbers from
+ # 1 to 10 without any remainder.
+#
+# What is the smallest positive number that is evenly divisible by
+# all of the numbers from 1 to 20?
+
 def divisible_by_1to20?(number)
   divisors = [19, 18, 17, 16, 15, 14, 13, 12, 11]
   divisors.each do |n|
@@ -15,3 +21,5 @@ until divisible_by_1to20?(number)
 end
 
 puts number
+
+# soution: 232792560
