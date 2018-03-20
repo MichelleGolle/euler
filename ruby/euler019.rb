55e9d5bd75726a1df8814a88e795e49e28d0b6d4
michelleambrisco <michelleg54@gmail.com>
2018-03-20 14:26:02 -0600

    refactor 19

--- ruby/euler019.rb
+++ ruby/euler019.rb
@@ -10,60 +10,14 @@
 # A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 # How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 require 'pry'
+require 'date'
 
 class Sunday
   def count
-    thirty_day_months = [4, 6, 9, 11]
-
-    month, day, year, weekday = 1, 1, 1900, 2 # weekdays are 1-7 so Monday is 2
-
-    def leap?(year)
-      return false if year == 1900
-      return true if year % 4 == 0
-    end
-
     first_sunday_count = 0
-
-    while year < 2001
-      if (weekday == 1 && day == 1 && year > 1900)
-        puts "******First sunday: #{month} #{day} #{year}"
-        first_sunday_count += 1
-      else
-        puts "month: #{month} day: #{day} weekday: #{weekday} year: #{year}"
-      end
-
-      case day
-        when 1..27
-          day += 1
-        when 28
-          if month == 2 && leap?(year)
-            day += 1
-          elsif month == 2 && !leap?(year)
-            day, month = 1, 3
-          else
-            day += 1
-          end
-        when 29
-          if month == 2
-            day, month = 1, 3
-          else
-            day += 1
-          end
-        when 30
-          if thirty_day_months.include?(month)
-            day, month = 1, month + 1
-          else
-            day += 1
-          end
-        when 31
-          if month == 12
-            month, day, year = 1, 1, year + 1
-          else
-            day, month = 1, month + 1
-          end
-      end
-      weekday += 1
-      weekday = 1 if weekday > 7 # go back to sunday after 7
+    dates = Date.new(1901, 01, 01).upto Date.new(2000, 12, 31)
+    dates.each do |date|
+      first_sunday_count += 1 if date.sunday? && date.mday == 1
     end
     first_sunday_count
   end
